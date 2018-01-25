73ec5f9230381cdb21aa9b5aed243722be43f138
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-25 19:19:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,23 @@
 def prime?(integer)
 
-factors = [] 
+factors = []
 (1...integer).each do |num|
-  if integer % num == 0 
-    factors << num 
+  if integer % num == 0
+    factors << num
+  end
+end
+  if factors == []
+    return true
+  else
+    false
   end 
-end 
-  if factors == [] 
-    return true 
-  else 
-    false 
-  end 
 
 
 
-  
+
 # x = 1
 # while x < integer
-#   integer.abs 
+#   integer.abs
 #   if integer % x == 0 && x != 1
 #     return false
 #   end
