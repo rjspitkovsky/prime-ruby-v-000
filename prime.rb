b1af7b1e5c9c7ad8ff9da2c0a222aefff955849c
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-25 19:19:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(integer)
+
+factors = [] 
+(1...integer).each do |num|
+  if integer % num == 0 
+    factors << num 
+  end 
+end 
+  if factors == [] 
+    return true 
+  else 
+    false 
+  end 
+
+
+
+  
+# x = 1
+# while x < integer
+#   integer.abs 
+#   if integer % x == 0 && x != 1
+#     return false
+#   end
+#   x += 1
+# end
+# true
+end
